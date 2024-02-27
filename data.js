501b6aff5662a8b56ccae9aea2921a15ec88eb72
capt <digitalindonesia01@gmail.com>
2024-02-28 02:16:55 +0700

    amsterdam cafe
    Virtual Tour Amsterdam Cafe

---
+++ data.js
@@ -0,0 +1,272 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-pintu-masuk",
+      "name": "Pintu Masuk",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "yaw": 0.07882240902767634,
+        "pitch": 0.16277490991663335,
+        "fov": 1.1490439151539336
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6477778790375623,
+          "pitch": 0.7624670506721998,
+          "rotation": 0,
+          "target": "1-spot-1"
+        },
+        {
+          "yaw": 0.003960672399884402,
+          "pitch": 0.630754777820643,
+          "rotation": 0,
+          "target": "2-spot-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.006667958678390207,
+          "pitch": 0.012738237027722477,
+          "title": "Selamat Datang di Amstedam Winkel &amp; Bistro",
+          "text": "Tempat nonkrong asik &amp; estetik. Pilih makananmu disini, nikmati suasana ala Belanda yang romantis banget. Jangan lupa foto-foto di sudut-sudut instragamable&nbsp; Amstedam Winkel &amp; Bistro.&nbsp;<div><br></div><div>Selamat Menikmati!</div>"
+        }
+      ]
+    },
+    {
+      "id": "1-spot-1",
+      "name": "Spot 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "yaw": 0.22276025674712585,
+        "pitch": 0.2199823919007926,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2098922264157412,
+          "pitch": 0.689593633916104,
+          "rotation": 0,
+          "target": "0-pintu-masuk"
+        },
+        {
+          "yaw": -0.4087412311597909,
+          "pitch": 0.6167527835926911,
+          "rotation": 0.7853981633974483,
+          "target": "2-spot-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-spot-2",
+      "name": "Spot 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "yaw": 1.0445606092916364,
+        "pitch": 0.23798534633197832,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.1603796928625858,
+          "pitch": 0.7145994838314067,
+          "rotation": 0,
+          "target": "0-pintu-masuk"
+        },
+        {
+          "yaw": 1.52069564619155,
+          "pitch": 0.696463933700656,
+          "rotation": 10.995574287564278,
+          "target": "1-spot-1"
+        },
+        {
+          "yaw": -1.6079634320315535,
+          "pitch": 0.5210658259412213,
+          "rotation": 0,
+          "target": "3-spot-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.7847411148449677,
+          "pitch": 0.47168732469312147,
+          "title": "Spot Instragamable",
+          "text": "Spot ini estetik banget ya. Kamu bisa foto ala-ala di Belanda gitu loh. Tentunya nambahin koleksi foto estetik di Instagram kamu.<div><br></div><div>Berikut contoh foto-fotonya, klik link di bawah ini ya!</div><div><br></div><div><a href=\"https://www.foodyas.com/ID/Bandung/1861551274081292/The-Amsterdam-Winkel-%26-Bistro\">https://www.foodyas.com/ID/Bandung/1861551274081292/The-Amsterdam-Winkel-%26-Bistro</a><br></div>"
+        }
+      ]
+    },
+    {
+      "id": "3-spot-3",
+      "name": "Spot 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "yaw": 2.1432397865021233,
+        "pitch": 0.1505276758109204,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.059673333872400036,
+          "pitch": 0.8349501340991914,
+          "rotation": 0,
+          "target": "2-spot-2"
+        },
+        {
+          "yaw": 1.7074699546409118,
+          "pitch": 0.6984620498788416,
+          "rotation": 0,
+          "target": "4-spot-4"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.3857842757137755,
+          "pitch": 0.3507434093369426,
+          "title": "Spot Instagramable",
+          "text": "Spot ini estetik banget ya. Kamu bisa foto ala-ala di Belanda gitu loh. Tentunya nambahin koleksi foto estetik di Instagram kamu.<div><br></div><div>Berikut contoh foto-fotonya, klik link di bawah ini ya!</div><div><br></div><div><a href=\"https://www.foodyas.com/ID/Bandung/1861551274081292/The-Amsterdam-Winkel-%26-Bistro\">https://www.foodyas.com/ID/Bandung/1861551274081292/The-Amsterdam-Winkel-%26-Bistro</a></div>"
+        }
+      ]
+    },
+    {
+      "id": "4-spot-4",
+      "name": "Spot 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "yaw": -1.4479468734542422,
+        "pitch": 0.2245483556657213,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4940740811906306,
+          "pitch": 0.8182254919027656,
+          "rotation": 0,
+          "target": "3-spot-3"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Amsterdam Winkel & Bistro",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
